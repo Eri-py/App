91d37c733aa706f7a27e9cf60cafdde540957671
Erioluwa Olajorin <eaolajorin06@gmail.com>
2025-06-23 17:49:31 +0100

    -Defined app color scheme -Defined registration route

--- Client/src/routes/__root.tsx
+++ Client/src/routes/__root.tsx
@@ -1,11 +1,23 @@
+import { ThemeProvider } from "@emotion/react";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { mainTheme } from "../themes/mainTheme";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
+  component: Root,
+});
+
+function Root() {
+  const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = mainTheme(isDarkMode);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Outlet />
       <TanStackRouterDevtools />
-    </>
-  ),
-});
+    </ThemeProvider>
+  );
+}
---
+++ Client/src/themes/mainTheme.ts
@@ -0,0 +1,56 @@
+import { createTheme } from "@mui/material/styles";
+
+export const mainTheme = (isDarkMode: boolean) =>
+  createTheme({
+    palette: {
+      mode: isDarkMode ? "dark" : "light",
+      primary: {
+        main: "#1565c0",
+        dark: "#0d47a1",
+        light: "#42a5f5",
+        contrastText: "#ffffff",
+      },
+      secondary: {
+        main: "#df2a2a",
+        dark: "#b11b1b",
+        light: "#ef5f5f",
+        contrastText: "#ffffff",
+      },
+      ...(isDarkMode
+        ? {
+            background: {
+              default: "#181818",
+              paper: "#1e1e1e",
+            },
+          }
+        : {
+            background: {
+              default: "#fafafa",
+              paper: "#ffffff",
+            },
+          }),
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: "none",
+            borderRadius: 8,
+          },
+        },
+      },
+      MuiFormHelperText: {
+        styleOverrides: {
+          root: {
+            fontSize: ".75rem",
+          },
+        },
+      },
+    },
+    typography: {
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      button: {
+        fontWeight: 500,
+      },
+    },
+  });
