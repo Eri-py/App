daf6deb4611d53152fd689c30761af3b55ee5165
Erioluwa Olajorin <eaolajorin06@gmail.com>
2025-07-15 21:23:30 +0100

    - Moved all Registration Dtos to one file to reduce clutter

--- Server/src/App.Api/Dtos/ResendVerificationCodeRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace App.Api.Dtos;
-
-public record class ResendVerificationCodeRequest
-{
-    [Required]
-    public required string Identifier { get; set; }
-}
