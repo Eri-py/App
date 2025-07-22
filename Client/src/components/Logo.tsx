d3b6785d2287c7822e048bdff536f3fbd717986d
Erioluwa Olajorin <eaolajorin06@gmail.com>
2025-07-22 12:59:25 +0100

    - Ui updates for login and sign up form.

--- Client/src/components/Logo.tsx
+++ Client/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
-export function Logo({ width }: { width: string }) {
+export function Logo({ width, color }: { width: string; color?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -12,15 +12,15 @@ export function Logo({ width }: { width: string }) {
     >
       <path
         d="M33.724 36.5809C37.7426 32.5622 40.0003 27.1118 40.0003 21.4286C40.0003 15.7454 37.7426 10.2949 33.724 6.27629C29.7054 2.25765 24.2549 1.02188e-06 18.5717 0C12.8885 -1.02188e-06 7.43807 2.25764 3.41943 6.27628L10.4905 13.3473C11.6063 14.4631 13.4081 14.4074 14.8276 13.7181C15.9836 13.1568 17.2622 12.8571 18.5717 12.8571C20.845 12.8571 23.0252 13.7602 24.6326 15.3677C26.2401 16.9751 27.1431 19.1553 27.1431 21.4286C27.1431 22.7381 26.8435 24.0167 26.2822 25.1727C25.5929 26.5922 25.5372 28.394 26.6529 29.5098L33.724 36.5809Z"
-        fill="#297AFF"
+        fill={color ?? "#297AFF"}
       />
       <path
         d="M30 40H19.5098C17.9943 40 16.5408 39.398 15.4692 38.3263L1.67368 24.5308C0.60204 23.4592 0 22.0057 0 20.4902V10L30 40Z"
-        fill="#34C2FF"
+        fill={color ?? "#34C2FF"}
       />
       <path
         d="M10.7143 39.9999H4.28571C1.91878 39.9999 0 38.0812 0 35.7142V29.2856L10.7143 39.9999Z"
-        fill="#34C2FF"
+        fill={color ?? "#34C2FF"}
       />
     </svg>
   );
@@ -28,13 +28,14 @@ export function Logo({ width }: { width: string }) {
 type LogoWithNameProps = {
   size: "small" | "medium" | "large";
   align?: string;
+  color?: string;
 };
-export function LogoWithName({ size, align }: LogoWithNameProps) {
-  const logoWithName = (iconSize: string, textSize: number) => {
+export function LogoWithName({ size, align, color }: LogoWithNameProps) {
+  const logoWithName = (iconSize: string, textSize: number, color?: string) => {
     return (
       <Stack direction="row" alignSelf={align ?? "unset"} gap={0.5}>
-        <Logo width={iconSize} />
-        <Typography color="primary" fontSize={textSize} fontWeight={700}>
+        <Logo width={iconSize} color={color} />
+        <Typography color={color ?? "primary"} fontSize={textSize} fontWeight={700}>
           App
         </Typography>
       </Stack>
@@ -43,10 +44,10 @@ export function LogoWithName({ size, align }: LogoWithNameProps) {
 
   switch (size) {
     case "small":
-      return logoWithName("18px", 16);
+      return logoWithName("18px", 16, color);
     case "medium":
-      return logoWithName("27px", 22);
+      return logoWithName("27px", 22, color);
     case "large":
-      return logoWithName("45px", 40);
+      return logoWithName("45px", 40, color);
   }
 }
